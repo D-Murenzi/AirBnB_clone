2b91ed65c61be0ecac1b234d3c495a26ed638724
D-Murenzi <murenzidavid01@gmail.com>
2024-02-12 12:56:49 +0200

    basemodel documentation collection

--- models/base_model.py
+++ models/base_model.py
@@ -6,21 +6,26 @@ import datetime as a
 
 class BaseModel():
     """define the common attributes and methods for other classes."""
+
     def __init__(self):
+        """Initialize the class instance."""
         self.id = str(uuid.uuid4())
         b = a.datetime.now()
         self.created_at = b
         self.updated_at = b
 
     def __str__(self):
+        """Return the string representation of the class instance."""
         return "[{:s}] ({:s}) {:}".format(
             self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """Update the time a change was made and store the changes."""
         c = a.datetime.now()
         self.updated_at = c
 
     def to_dict(self):
+        """Return a string representation of the object."""
         created = self.created_at.isoformat()
         updated = self.updated_at.isoformat()
         in_dict = self.__dict__.copy()

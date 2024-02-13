2249b8fc377702195dbd4fc9140564691860ab4f
D-Murenzi <murenzidavid01@gmail.com>
2024-02-13 10:01:42 +0200

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,26 @@ import datetime as a
 class BaseModel():
     """define the common attributes and methods for other classes."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize the class instance."""
-        self.id = str(uuid.uuid4())
-        b = a.datetime.now()
-        self.created_at = b
-        self.updated_at = b
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    self.created_at = a.datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == 'updated_at':
+                    self.updated_at = a.datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            b = a.datetime.now()
+            self.created_at = b
+            self.updated_at = b
 
     def __str__(self):
-        """Return the string representation of the class instance."""
+        """Return string representation of the object."""
         return "[{:s}] ({:s}) {:}".format(
             self.__class__.__name__, self.id, self.__dict__)
 
